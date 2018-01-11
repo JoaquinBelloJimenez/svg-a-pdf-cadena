2dc1cf6cac7c437415f56d70d6233c0edc9f7949
Joaquín <joaquin.bj.97@gmail.com>
2018-01-11 09:51:48 +0100

    Versión Final

---
+++ svg_a_pdf.bat
@@ -0,0 +1,46 @@
+:10-01-2018
+:Joaquin Bello Jimenez
+
+@ECHO OFF
+TITLE svg a pdf
+
+SET /A "cont=0"
+
+
+
+
+:Leer los archivos de la capeta
+FOR %%G IN (*.svg) DO (
+	ECHO %%G
+	SET /A "cont=cont+1"
+	)
+
+IF "%cont%" GEQ "1" (
+	GOTO procesar_documentos
+	)
+
+:Si no existen archivos, salir
+	ECHO Documentos SVG no encontrados
+	ECHO Pulsa cualquier tecla para salir. . .
+	GOTO salir
+
+
+:procesar_documentos
+ECHO Convirtiendo SVG a PDF
+ECHO Espere un momento, por favor...
+
+:Generar la carpeta PDF
+if NOT EXIST "pdf\" MKDIR pdf
+
+:Generar todos los archivos pdf de la carpeta
+FOR %%G IN (*.svg) DO (
+	START inkscape %%G -A pdf/%%~nG.pdf
+	)
+
+:Fin del programa
+ECHO.
+ECHO Documentos convertidos satisfactoriamente :)
+ECHO.
+ECHO Pulsa cualquier tecla para continuar. . .
+:salir
+PAUSE >NUL
